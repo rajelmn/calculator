bfe12c1a1105b634b17d5e368440f751cfe39b30
brahim <139687519+rajelmn@users.noreply.github.com>
2024-07-14 17:51:28 +0000

    solving problems and adding more feautures
    in this version i solved the wrong calculation , but there still an issue where you cant calculate 3 times in a row , e.g if you calculate 2-2-2 , the only last 2-2 will be counted and the whole operation will return 0 , so i will fix it soon

--- simple-calc/script.js
+++ simple-calc/script.js
@@ -1,9 +1,13 @@
-let numbers = ['zero','one', 'two', 'three' , 'four', 'five', 'six', 'seven', 'eight', 'nine', 'plus', 'equal', 'minus'];
+let numbers = ['zero','one', 'two', 'three' , 'four', 'five', 'six', 'seven', 'eight', 'nine', 'plus', 'equal', 'minus', 'divide', 'mul', 'modulo', 'AC', 'DEL'];
 let input = document.getElementById('calculator-output');
 
-
+let isMinus = false;
+let isMul = false;
+let isPlus = false;
+let isDivide = false;
+let isModulo = false;
 const value = '';
-let i = 0;
+
 input.value = value;
 let result = 0;
 numbers.forEach( number => {
@@ -40,21 +44,63 @@ numbers.forEach( number => {
        input.value += 9;
        break;
        case 'plus':
-        //  input.value = Number(input.value);
-        result += Number(input.value);
+         result = Number(input.value)
          input.value = '';
-         console.log(result)
+         isPlus = true;
          break;
          case 'minus':
-            result -= Number(input.value);
+            result = Number(input.value);
             input.value = '';
+            isMinus = true;
             break
+        case 'mul': 
+           result = Number(input.value);
+           isMul = true;
+           input.value = '';
+           break;
+        case 'divide': 
+           result = Number(input.value);
+           isDivide = true;
+           input.value = '';
+           break;
+        case 'modulo':
+            result = Number(input.value);
+            isModulo = true;
+            input.value = '';
+            break;
+        case 'AC':
+            input.value = '';
+            result = 0;
+            break;
+        case 'DEL': 
+       input.value = (input.value).slice(0 , (input.value).length - 1);
+        
+        break;
        case 'equal':
-         console.log(result);
-         console.log(input.value)
-         if(result) input.value = result + Number(input.value);   
-         console.log(input.value)      
+              if(isPlus) {
+                input.value = result + Number(input.value);
+                result = +input.value;
+                isPlus = false;
+              }
+              else if(isMinus) {
+                input.value =result - input.value;
+                result = +input.value;
+                isMinus = false;
+              }
+              else if (isMul) {
+                input.value = result * input.value;
+                isMul = false
+              }
+              else if(isDivide) {
+                input.value = result / input.value;
+                isMul = false;
+              }
+              else if(isModulo) {
+                input.value = result % input.value;
+                isModulo = false;
+              }
      }
    })
 })
+
 
